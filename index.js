2202fbd34a92500472897f57bb5b7a0b84631882
Rakotonaivo Rado Tiako Sombiniaina <sombitiako@gmail.com>
2022-05-11 22:58:50 +0300

    Initialisation environment

---
+++ index.js
@@ -0,0 +1,13 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
